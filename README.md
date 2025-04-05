4ddb0222fdb2f79002c339deadec8b6c83a15d7c
omnitrix <shubhshree118@gmail.com>
2025-04-05 23:45:07 +0530

    new content

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # project-clone
+<br>
+what is your name!?
+how can i help you?
---
+++ index.js
@@ -0,0 +1,4 @@
+let a=['human',"cat","dog","mouse","elephant","tiger","lion","zebra","giraffe","monkey"];
+console.log(a[0]);
+let b=[1,2,3,4,5,6,7,8,9,10];
+console.log(b[0]);
