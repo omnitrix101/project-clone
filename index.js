844dda0bf46524c9ea648575591d8aabd96135b1
omnitrix101 <shubhshree1118@gmail.com>
2025-04-05 18:32:14 +0000

    new work

--- index.js
+++ index.js
@@ -2,3 +2,5 @@ let a=['human',"cat","dog","mouse","elephant","tiger","lion","zebra","giraffe","
 console.log(a[0]);
 let b=[1,2,3,4,5,6,7,8,9,10];
 console.log(b[0]);
+let c=[true,false,true,false,true,false,true,false,true,false];
+console.log(c[0]);
